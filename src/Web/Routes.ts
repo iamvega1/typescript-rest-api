0eb22d46a7a89a0a931b1de6d8137271e3713f37
Donny Roufs <iamchets@yahoo.com>
2021-03-11 01:12:19 +0100

    added validation :sparkles:

--- src/Web/Routes.ts
+++ src/Web/Routes.ts
@@ -3,18 +3,26 @@ import Path from 'path'
 import { Router as ExpressRouter } from 'express'
 import { Browter } from '@donnyroufs/browter'
 import { ExpressToBrowterAdapter } from '@donnyroufs/express-to-browter-adapter'
+import { createValidator } from 'express-joi-validation'
+import { SubscribersValidation } from './Api/Validators/Subscribers.validator'
 
 const expressAdapter = new ExpressToBrowterAdapter(ExpressRouter)
 const browter = new Browter<ExpressRouter>(expressAdapter, {
   controllersDir: Path.resolve('./src/Web/Api/Controllers'),
 })
 
+const validator = createValidator()
+
 browter.group(`/api/v1`, (browter) => {
   browter.group('/subscribers', (browter) => {
     browter.get('/', 'SubscribersController.index')
     browter.get('/:id', 'SubscribersController.show')
-    browter.post('/', 'SubscribersController.store')
-    browter.patch('/:id', 'SubscribersController.update')
+    browter.post('/', 'SubscribersController.store', [
+      validator.body(SubscribersValidation.store()),
+    ])
+    browter.patch('/:id', 'SubscribersController.update', [
+      validator.body(SubscribersValidation.update()),
+    ])
     browter.delete('/:id', 'SubscribersController.destroy')
   })
 })
